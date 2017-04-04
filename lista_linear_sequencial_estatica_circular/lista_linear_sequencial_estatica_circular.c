35d7db2667b6a0ed845f6271907967a94a7859c8
Tony Alexander Hild <tony_hild@yahoo.com>
2017-04-03 23:19:58 -0300

    Adicionado lista circular

---
+++ lista_linear_sequencial_estatica_circular/lista_linear_sequencial_estatica_circular.c
@@ -0,0 +1,135 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lista_linear_sequencial_estatica_circular.h"
+
+list *new_list(int size)
+{
+    list *l = (list *)malloc(sizeof(list));
+    l->items = (int *)malloc(size * sizeof(int));
+    l->count = 0;
+    l->first = -1;
+    l->last = -1;
+    l->max = size;
+    return l;
+}
+
+int is_null(list *l)
+{
+    return l == NULL;
+}
+
+void clear(list *l)
+{
+    if (is_null(l))
+        return;
+    l->count = 0;
+    l->first = -1;
+    l->last = -1;
+}
+
+// list.c
+void insert_last(int x, list *l)
+{
+    if (is_null(l))
+        return;
+    insert_at(x, l->last, l);
+}
+
+int remove_at(int i, list *l)
+{
+    if (is_null(l) ||
+        is_empty(l) ||
+        (i < 0 || i > l->last))
+        return -1;
+    int x = l->items[i];
+    for (int j = i; j < l->last; j++)
+    {
+        l->items[j] = l->items[j + 1];
+    }
+    l->last--;
+    l->count--;
+    if (is_empty(l))
+    {
+        l->first = -1;
+    }
+    return x;
+}
+
+int is_empty(list *l)
+{
+    return is_null(l) || l->count == 0;
+}
+
+int is_full(list *l)
+{
+    return l->count == l->max;
+}
+
+void print(list *l)
+{
+    for (int i = 0; i < l->count; i++)
+    {
+        printf("Item[%d] = %d\n", i, l->items[i]);
+    }
+}
+
+void destroy_list(list **l)
+{
+    free((*l)->items);
+    free(*l);
+    *l = NULL;
+}
+
+//Remove o último elemento e o retorna
+int remove_last(list *l)
+{
+    if (is_null(l))
+        return -1;
+    return remove_at(l->last, l);
+}
+
+//Remove o primeiro elemento e o retorna
+int remove_first(list *l)
+{
+    if (is_null(l))
+        return -1;
+    return remove_at(l->first, l);
+}
+
+//Insere o elemento x na posição i
+void insert_at(int x, int i, list *l)
+{
+    if (is_null(l) ||
+        is_full(l) ||
+        (i < 0 || i > l->last))
+        return;
+
+    for (int j = l->last; j >= i; j--)
+    {
+        l->items[j + 1] = l->items[j];
+    }
+    l->last++;
+    l->items[i] = x;
+    l->count++;
+}
+
+//Insere um elemento na primeira posição
+void insert_first(int x, list *l)
+{
+    if (is_null(l))
+        return;
+    insert_at(x, l->first, l);
+}
+
+//Procura por um elemento e retorna sua posição
+int search(int x, list *l)
+{
+    if (is_null(l) || is_empty(l))
+        return -1;
+    for (int i = l->first; i < l->last; i++)
+    {
+        if (x == l->items[i])
+            return i;
+    }
+    return -1;
+}
---
+++ lista_linear_sequencial_estatica_circular/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lista_linear_sequencial_estatica_circular.h"
+
+void main() {
+    list *l = new_list(10);
+    insert_last(0, l);
+    insert_last(10, l);
+    insert_last(20, l);
+    insert_last(30, l);
+    insert_last(40, l);
+    insert_last(50, l);
+    printf("Inserido 0, 10, 20, 30, 40, 50\n");
+    print(l);
+    printf("Item %d removido da posicao 2.\n", remove_at(2, l));
+    print(l);
+    printf("Item %d removido da primeira posicao.\n", remove_first(l));
+    print(l);
+    printf("Item %d removido da ultima posicao.\n", remove_last(l));
+    print(l);
+    printf("Inserido 90 na posição 1\n");
+    insert_at(90, 1, l);
+    print(l);
+    printf("Inserido 80 na primeira posicao\n");
+    insert_first(80, l);
+    print(l);
+    destroy_list(&l);
+    printf("Lista destruida e sem dangling pointers? %s\n", l == NULL ? "Sure!" : "Wah Wah Wah Waaaaaaaah!");
+}
